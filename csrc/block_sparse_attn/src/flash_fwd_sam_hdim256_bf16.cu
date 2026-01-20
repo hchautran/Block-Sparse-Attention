d6a25ff2c6163d5df21ba61bab90efdb1b60af9d
ducido.w@gmail.com <ducido>
2026-01-20 16:51:20 +0000

    update

---
+++ csrc/block_sparse_attn/src/flash_fwd_sam_hdim256_bf16.cu
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Copyright (c) 2024, Tri Dao.
+ * Adapted by Junxian Guo.
+ * SAM-only kernel instantiation for head_dim=256, bf16.
+ ******************************************************************************/
+
+#include "namespace_config.h"
+#include "flash_fwd_launch_sam.h"
+
+namespace FLASH_NAMESPACE {
+
+// Template instantiation for bf16, head_dim=256
+template<>
+void run_mha_fwd_sam_<cutlass::bfloat16_t, 256>(Flash_fwd_sam_params &params, cudaStream_t stream) {
+    run_mha_fwd_sam_hdim256<cutlass::bfloat16_t>(params, stream);
+}
+
+}  // namespace FLASH_NAMESPACE
